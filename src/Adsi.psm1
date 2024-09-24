cdb9177fdce73f718506bc825d8306b5f430ad2e
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-09-24 00:01:48 -0700

    add debug pause back

--- src/Adsi.psm1
+++ src/Adsi.psm1
@@ -240,3 +240,4 @@ Export-ModuleMember -Function @('Add-DomainFqdnToLdapPath','Add-SidInfo','Conver
 
 
 
+
--- src/functions/public/Get-DirectoryEntry.ps1
+++ src/functions/public/Get-DirectoryEntry.ps1
@@ -127,6 +127,7 @@ function Get-DirectoryEntry {
     } else {
         $Server = $ParentPath
     }
+    if ($Server -ne 'JLA-LoftHTPC') { pause }
     $CimServer = $CimCache[$Server]
 
     <#
