ae8c19091d63044fe38263c288b0f5bc48262d73
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-09-30 19:30:52 -0700

    use math instead of sc.exe for service sid calculation

--- dist/4.0.220/Adsi/Adsi.psm1
+++ dist/4.0.220/Adsi/Adsi.psm1
@@ -6464,6 +6464,36 @@ function Resolve-ServiceNameToSID {
 
         ForEach ($Svc in $InputObject) {
 
+            #2: Convert service name to upper case.
+            $UppercaseName = $Svc.Name.ToUpper()
+
+            #3: Get the Unicode bytes()  from the upper case service name.
+            $nameBytes = [System.Text.Encoding]::Unicode.GetBytes($UppercaseName)
+
+            #4: Run bytes() thru the sha1 hash function.
+            $hashBytes = ([System.Security.Cryptography.SHA1]::Create()).ComputeHash($nameBytes, 0, $nameBytes.Length)
+
+            #5: Reverse the byte() string returned from the SHA1 hash function (on Little Endian systems Not tested on Big Endian systems)
+            [Array]::Reverse($hashBytes)
+            [string[]]$hashString = $hashBytes | % { $_.ToString("X2") }
+
+            #6: Split the reversed string into 5 blocks of 4 bytes each.
+            $blocks = @()
+            for ($i = 0; $i -lt 5; $i++) {
+
+                #7: Convert each block of hex bytes() to Decimal
+                $blocks += [Convert]::ToInt64("0x$([String]::Join([String]::Empty, $hashString, ($i * 4), 4))", 16)
+            }
+
+            #8: Reverse the Position of the blocks
+            [Array]::Reverse($blocks)
+
+            #9: Create the first part of the SID “S-1-5-80“
+            #10: Tack on each block of Decimal strings with a “-“ in between each block that was converted and reversed.
+            #11: Finally out put the complete SID for the service.
+            $SID = "S-1-5-80-$([String]::Join("-", $blocks))"
+
+            <#
             #Write-LogMsg @Log -Text "Invoke-ScShowSid -ServiceName '$($Svc.Name)' -ComputerName '$ComputerName' -ThisFqdn '$ThisFqdn' -ThisHostName '$ThisHostName' -Log `$Log"
             $ScShowSidResults = Invoke-ScShowSid -ServiceName $Svc.Name -ComputerName $ComputerName -ThisFqdn $ThisFqdn -ThisHostName $ThisHostName -Log $Log
             $ServiceSidAndStatus = ConvertFrom-ScShowSidResult -Result $ScShowSidResults
@@ -6472,6 +6502,12 @@ function Resolve-ServiceNameToSID {
                 Name   = $Svc.Name
                 SID    = $ServiceSidAndStatus.'SERVICE SID'
                 Status = $ServiceSidAndStatus.Status
+            }
+            #>
+
+            $OutputObject = @{
+                Name = $Svc.Name
+                SID  = $SID
             }
 
             ForEach ($Prop in ($Svc | Get-Member -View All -MemberType Property, NoteProperty).Name) {
@@ -6639,6 +6675,7 @@ ForEach ($ThisFile in $CSharpFiles) {
 }
 #>
 Export-ModuleMember -Function @('Add-DomainFqdnToLdapPath','Add-SidInfo','ConvertFrom-DirectoryEntry','ConvertFrom-IdentityReferenceResolved','ConvertFrom-PropertyValueCollectionToString','ConvertFrom-ResultPropertyValueCollectionToString','ConvertFrom-SearchResult','ConvertFrom-SidString','ConvertTo-DecStringRepresentation','ConvertTo-DistinguishedName','ConvertTo-DomainNetBIOS','ConvertTo-DomainSidString','ConvertTo-Fqdn','ConvertTo-HexStringRepresentation','ConvertTo-HexStringRepresentationForLDAPFilterString','ConvertTo-SidByteArray','Expand-AdsiGroupMember','Expand-WinNTGroupMember','Find-AdsiProvider','Find-LocalAdsiServerSid','Get-ADSIGroup','Get-ADSIGroupMember','Get-AdsiServer','Get-CurrentDomain','Get-DirectoryEntry','Get-KnownCaptionHashTable','Get-KnownSid','Get-KnownSidHashtable','Get-ParentDomainDnsName','Get-TrustedDomain','Get-WinNTGroupMember','Invoke-ComObject','New-FakeDirectoryEntry','Resolve-IdentityReference','Resolve-ServiceNameToSID','Search-Directory')
+
 
 
 
--- src/functions/public/Resolve-ServiceNameToSID.ps1
+++ src/functions/public/Resolve-ServiceNameToSID.ps1
@@ -18,6 +18,36 @@ function Resolve-ServiceNameToSID {
 
         ForEach ($Svc in $InputObject) {
 
+            #2: Convert service name to upper case.
+            $UppercaseName = $Svc.Name.ToUpper()
+
+            #3: Get the Unicode bytes()  from the upper case service name.
+            $nameBytes = [System.Text.Encoding]::Unicode.GetBytes($UppercaseName)
+
+            #4: Run bytes() thru the sha1 hash function.
+            $hashBytes = ([System.Security.Cryptography.SHA1]::Create()).ComputeHash($nameBytes, 0, $nameBytes.Length)
+
+            #5: Reverse the byte() string returned from the SHA1 hash function (on Little Endian systems Not tested on Big Endian systems)
+            [Array]::Reverse($hashBytes)
+            [string[]]$hashString = $hashBytes | % { $_.ToString("X2") }
+
+            #6: Split the reversed string into 5 blocks of 4 bytes each.
+            $blocks = @()
+            for ($i = 0; $i -lt 5; $i++) {
+
+                #7: Convert each block of hex bytes() to Decimal
+                $blocks += [Convert]::ToInt64("0x$([String]::Join([String]::Empty, $hashString, ($i * 4), 4))", 16)
+            }
+
+            #8: Reverse the Position of the blocks
+            [Array]::Reverse($blocks)
+
+            #9: Create the first part of the SID “S-1-5-80“
+            #10: Tack on each block of Decimal strings with a “-“ in between each block that was converted and reversed.
+            #11: Finally out put the complete SID for the service.
+            $SID = "S-1-5-80-$([String]::Join("-", $blocks))"
+
+            <#
             #Write-LogMsg @Log -Text "Invoke-ScShowSid -ServiceName '$($Svc.Name)' -ComputerName '$ComputerName' -ThisFqdn '$ThisFqdn' -ThisHostName '$ThisHostName' -Log `$Log"
             $ScShowSidResults = Invoke-ScShowSid -ServiceName $Svc.Name -ComputerName $ComputerName -ThisFqdn $ThisFqdn -ThisHostName $ThisHostName -Log $Log
             $ServiceSidAndStatus = ConvertFrom-ScShowSidResult -Result $ScShowSidResults
@@ -26,6 +56,12 @@ function Resolve-ServiceNameToSID {
                 Name   = $Svc.Name
                 SID    = $ServiceSidAndStatus.'SERVICE SID'
                 Status = $ServiceSidAndStatus.Status
+            }
+            #>
+
+            $OutputObject = @{
+                Name = $Svc.Name
+                SID  = $SID
             }
 
             ForEach ($Prop in ($Svc | Get-Member -View All -MemberType Property, NoteProperty).Name) {
