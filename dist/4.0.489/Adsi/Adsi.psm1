21dc1a744c5512f83d987c7b2d9a350bf8692176
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-11-30 15:52:44 -0800

    rebuild due to src ctrl failure

--- dist/4.0.489/Adsi/Adsi.psm1
+++ dist/4.0.489/Adsi/Adsi.psm1
@@ -6915,3 +6915,4 @@ Export-ModuleMember -Function @('Add-DomainFqdnToLdapPath','Add-SidInfo','Conver
 
 
 
+
--- src/Adsi.psm1
+++ src/Adsi.psm1
@@ -556,3 +556,4 @@ Export-ModuleMember -Function @('Add-DomainFqdnToLdapPath','Add-SidInfo','Conver
 
 
 
+
